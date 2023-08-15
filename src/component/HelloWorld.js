c597c6d053046d02a8e5c827ffbf144686147173
Chidera Anthony <geanthony94@gmail.com>
2023-08-15 12:48:48 -0700

    cleaned up file

---
+++ src/component/HelloWorld.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function HelloWorld() {
+  return (
+    <nav>
+      <img
+        src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
+        alt=""/>
+      <h3>ReactFacts</h3>
+      <h4>Raect Course - Project 1</h4>
+    </nav>
+  );
+}
+
+export default HelloWorld
---
+++ src/component/main.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Main() {
+  return (
+    <main>
+      <h1 className="fact">Fun facts about React</h1>
+      <ul className="list">
+        <li>Was frist released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 200k starts on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Power thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
+}
+
+export default Main;
